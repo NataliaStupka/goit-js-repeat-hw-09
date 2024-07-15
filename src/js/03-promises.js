d4d562acb5f98f3e9e1e1a3416c0c8dcdfc1aea1
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2024-07-15 16:38:34 +0300

    Update 03-promises.js
    task#3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,7 +8,23 @@ let VALUE_AMOUNT = null;
 
 let count = null;
 
+//-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+  setTimeout(() => {
+    if (shouldResolve) {
+      resolve({position, delay});
+         } else {
+      reject({position, delay});
+        }
+  }, delay);
+});}
 //-------------------------------------------------------------
+
 promiseForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
@@ -20,73 +36,18 @@ function handleSubmit(event) {
   VALUE_AMOUNT = form.elements.amount.value;
   let step = Number(VALUE_FIRST_DELAY);
 
-  setTimeout(() => {
-    const setIntervalId = setInterval(() => {
-      count += 1;
+  console.log('CLICK handleSubmit')
 
-      if (count > VALUE_AMOUNT) {
-        clearInterval(setIntervalId);
-        Notify.warning('Припиняємо інтервал');
-        count = 0;
-        console.log('ОБНУЛЯЄМО count');
-        return;
-      }
-
-      console.log(`Викликаємо функцію createPromise: '${count}'`);
-
-      //console.log('STEP::', step);
-
-      createPromise(count, step);
-
-      step += Number(VALUE_DELAY_STEP);
-    }, step);
-  }, VALUE_FIRST_DELAY);
-}
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  // console.log('POSITION:', position);
-  // console.log('DELAY:', delay);
-  if (shouldResolve) {
-    // Fulfill
-    Notify.success(`✅ Fulfilled promise ${position} in  ${delay}ms`);
-    console.log(`✅ Fulfilled promise '${position}' in - ${delay}ms`);
-  } else {
-    // Reject
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-    console.log(`❌ Rejected promise '${position}' in - ${delay}ms`);
+  for(let i = 1; i <= VALUE_AMOUNT; i += 1) {
+   const prom = createPromise(i, step);
+   prom.then(({position, delay}) => {
+    Notify.success(`✅ Fulfilled promise '${position}' in - ${delay}ms`)
+   }).catch(({position, delay}) => {
+    return Notify.failure(`❌ Rejected promise '${position}' in - ${delay}ms`)
+   })
+   step += Number(VALUE_DELAY_STEP);
   }
 }
-//--------------------------------
 
-const createPromiseP = new Promise((resolve, reject) => {
-  const shouldResolve = Math.random() > 0.3;
-  setTimeout(() => {
-    console.log('ТУТ ПРОМІС');
 
-    if (shouldResolve) {
-      resolve('Success!');
-    } else {
-      reject('Error!');
-    }
-  }, 5000);
-});
 
-createPromiseP
-  .then(({ position, delay }) => {
-    console.log(`✅ '2'Fulfilled promise '${position}' in '${delay}'ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ '2'Rejected promise ${position} in ${delay}ms`);
-  });
-
-//--------------------------------------
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-//---*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-------
